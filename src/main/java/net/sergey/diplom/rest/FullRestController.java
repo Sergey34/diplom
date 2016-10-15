0b0b7de56f0d45d7872281aba5fc85c4ee7954b3
sergey <sergey3440@gmail.com>
2016-10-15 20:29:23 +0300

    запуск python скрипта

--- src/main/java/net/sergey/diplom/rest/FullRestController.java
+++ src/main/java/net/sergey/diplom/rest/FullRestController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 
@@ -12,9 +16,18 @@ import java.util.HashMap;
 public class FullRestController {
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public HashMap<String, Integer> greeting() {
-        System.out.println("###############");
-        HashMap<String, Integer> stringIntegerHashMap = new HashMap<String, Integer>();
+    public HashMap<String, Integer> greeting() throws IOException, InterruptedException {
+        String args = "aargs";
+        Process p = Runtime.getRuntime().exec("python3 /home/sergey/workspace/IdeaProjects/diplom/diplom/src/main/resources/myscript.py " + args);
+        int i = p.waitFor();
+        System.out.println(i);
+
+        File file = new File("/home/sergey/workspace/IdeaProjects/diplom/diplom/src/main/resources/" + args + ".txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String s = bufferedReader.readLine();
+        System.out.println(s);
+
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
         stringIntegerHashMap.put("qwe1", 123);
         stringIntegerHashMap.put("qwe2", 1232);
         stringIntegerHashMap.put("qwe3", 1233);
