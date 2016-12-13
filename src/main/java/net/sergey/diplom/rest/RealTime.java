61da7a7f49d4b2230acae7a7cc6b71f760e7ee4c
sergey <sergey3440@gmail.com>
2016-12-13 23:22:10 +0400

    подготовка к SSE (работает) необходимо реализовать саму логику

--- src/main/java/net/sergey/diplom/rest/RealTime.java
+++ src/main/java/net/sergey/diplom/rest/RealTime.java
@@ -1,18 +1,23 @@
 package net.sergey.diplom.rest;
 
-import org.springframework.stereotype.Controller;
+import net.sergey.diplom.domain.user.User;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class RealTime {
 
     private final List<SseEmitter> emitters = new ArrayList<>();
 
 
-  /*  @RequestMapping(value = "/getRealTimeMessage")
+    @RequestMapping(value = "/getRealTimeMessage")
     public SseEmitter getRealTimeMessageAction() throws IOException {
         final SseEmitter emitter = new SseEmitter();
         emitters.add(emitter);
@@ -23,23 +28,23 @@ public class RealTime {
             }
         });
         return emitter;
+    }
 
-    }*/
-
-    /*
-    @RequestMapping(path = "/chat", method = RequestMethod.POST)
-    public String sendMessage() {
+    @RequestMapping(path = "/chat", method = RequestMethod.GET)
+    public Object sendMessage() {
+        User user = new User();
         for (SseEmitter emitter : emitters) {
             try {
-                emitter.send("message", MediaType.APPLICATION_JSON);
+                user.setId(12);
+                user.setUserName("qeqwe");
+                emitter.send(user, MediaType.APPLICATION_JSON);
             } catch (IOException e) {
                 emitter.complete();
                 emitters.remove(emitter);
                 e.printStackTrace();
             }
         }
-        return "message";
+        return user;
     }
-*/
 
 }
