fffdd4ef5cd0ee9d44f71165997dba6fa8aae37f
sergey <sergey3440@gmail.com>
2016-12-15 22:15:42 +0400

    переработал html

--- src/main/webapp/static/js/get_detail_info.js
+++ src/main/webapp/static/js/get_detail_info.js
@@ -1,4 +1,4 @@
-var id;
+let id;
 function getDetailInfo(forEdit) {
     id = $.getUrlVar('airfoilId');
     if (id == undefined || id == '') {
@@ -23,142 +23,77 @@ function getDetailInfo(forEdit) {
 }
 
 function fillContentDetailInfo(data) {
-    var adf = document.createElement('div');
-    adf.id = 'airfoilDetailInfo';
-
+    let adf = document.getElementById('airfoilDetailInfo');
     if (data.warnMessage = undefined) {
         adf.innerHTML = warnMessage;
     }
 
-    var edit = document.createElement('a');
+    let edit = document.getElementById('edit_link');
     edit.setAttribute("href", "/editAirfoil?airfoilId=" + id);
-    edit.innerHTML = 'Редактировать';
-    adf.appendChild(edit);
 
-    var name = document.createElement('div');
-    extracted(name, data.name, 'name_detail');
-    var stlFilePath = document.createElement('div');
-    var linkItem = document.createElement('a');
-    linkItem.setAttribute("href", data.stlFilePath);
-    linkItem.innerHTML = 'Скачать STL файл';
-    stlFilePath.appendChild(linkItem);
+    let name = document.getElementById('name_detail');
+    name.innerText = data.name;
 
-    var image = document.createElement('div');
-    var img = document.createElement('img');
-    img.setAttribute("src", data.image);
-    extracted(image, '', 'img_detail');
-    image.appendChild(img);
+    let downloadStl = document.getElementById('downloadStl');
+    downloadStl.setAttribute("href", data.stlFilePath);
 
-    var description = document.createElement('div');
-    extracted(description, data.description, 'descr_detail');
+    let image = document.getElementById('imgDetail');
+    image.setAttribute("src", data.image);
 
-    var fileCsvName = data.fileCsvName;
-    var links = document.createElement('div');
-    var csvRenolds = document.createElement('div');
-    csvRenolds.innerHTML = "Renolds";
-    csvRenolds.className = "renolgs";
-    links.appendChild(csvRenolds);
-    var NCrit = document.createElement('div');
-    NCrit.innerHTML = "NCrit";
-    NCrit.className = "NCrit";
-    links.appendChild(NCrit);
-    var MaxClCd = document.createElement('div');
-    MaxClCd.innerHTML = "MaxClCd";
-    MaxClCd.className = "MaxClCd";
-    links.appendChild(MaxClCd);
-    links.appendChild(document.createElement('br'));
 
-    for (var i = 0; i < fileCsvName.length; i++) {
-        var csvItem = document.createElement('div');
-        var csvItemChBox = document.createElement('input');
-        csvItemChBox.setAttribute("type", "checkbox");
-        csvItemChBox.setAttribute("class", "csvItemChBox");
-        csvItemChBox.setAttribute("id", fileCsvName[i].fileName);
-        csvItemChBox.setAttribute("name", "activ");
-        csvItemChBox.setAttribute("checked", "checked");
-        csvItem.appendChild(csvItemChBox);
+    let description = document.getElementById('descr_detail');
+    description.innerText = data.description;
 
-        var csvRenolds = document.createElement('div');
+    let fileCsvName = data.fileCsvName;
+
+    for (let i = 0, j = 1; i < fileCsvName.length; i++, j++) {
+
+        let csvItemChBox = document.getElementById('checkbox' + j);
+        csvItemChBox.setAttribute("fileName", fileCsvName[i].fileName);
+
+        let csvRenolds = document.getElementById('Renolds' + j);
         csvRenolds.innerHTML = fileCsvName[i].renolgs;
-        csvRenolds.className = "renolgs";
-        csvItem.appendChild(csvRenolds);
-        var csvNCrit = document.createElement('div');
+
+        let csvNCrit = document.getElementById('NCrit' + j);
         csvNCrit.innerHTML = fileCsvName[i].nCrit;
-        csvNCrit.className = "NCrit";
-        csvItem.appendChild(csvNCrit);
-        var csvMaxClCd = document.createElement('div');
+
+        let csvMaxClCd = document.getElementById('MaxClCd' + j);
         csvMaxClCd.innerHTML = fileCsvName[i].maxClCd;
-        csvMaxClCd.className = "MaxClCd";
-        csvItem.appendChild(csvMaxClCd);
 
-        var fileName = document.createElement('div');
-        fileName.className = "fileName";
-        var linkItem = document.createElement('a');
+        let linkItem = document.getElementById('link_file' + j);
         linkItem.setAttribute("href", "/resources/tmpCsv/" + fileCsvName[i].fileName);
-        linkItem.innerHTML = fileCsvName[i].fileName;
-        fileName.appendChild(linkItem);
-        csvItem.appendChild(fileName);
-        links.appendChild(csvItem);
-        links.appendChild(document.createElement('br'))
+        linkItem.innerText = fileCsvName[i].fileName;
     }
-    var btnUpdatePlot = document.createElement('input');
-    btnUpdatePlot.setAttribute("type", "button");
-    btnUpdatePlot.setAttribute("value", "Обновить");
-    btnUpdatePlot.setAttribute("onclick", "refreshiframe();");
-    links.appendChild(btnUpdatePlot)
 
-    var imgCsvName = data.imgCsvName;
-    var imgCsvBox = document.createElement('div');
-    imgCsvBox.id = "imgCsvBox";
-    for (var i = 0; i < imgCsvName.length; i++) {
-        var imgCsv = document.createElement('div');
-        var img = document.createElement('img');
+    let imgCsvName = data.imgCsvName;
+    let imgCsvBox = document.getElementById('imgCsvBox');
+    for (let i = 0; i < imgCsvName.length; i++) {
+        let imgCsv = document.createElement('div');
+        let img = document.createElement('img');
         img.setAttribute('src', imgCsvName[i]);
         imgCsv.appendChild(img);
         imgCsvBox.appendChild(imgCsv);
     }
-
-
-    adf.appendChild(name);
-    adf.appendChild(stlFilePath);
-    adf.appendChild(image);
-    adf.appendChild(description);
-    adf.appendChild(links);
-    adf.appendChild(imgCsvBox);
-
-    // добавление в конец
-    document.getElementById('contentid').appendChild(adf);
-}
-function extracted(element, value, className) {
-    element.innerHTML = value;
-    element.setAttribute('class', className);
 }
 
-function createNewInput(name, value) {
-    var input = document.createElement('input');
-    input.value = value;
-    input.name = name;
-    input.setAttribute('type', 'text');
-    return input;
-}
 function fillEditableContentDetailInfo(data) {
-    var adf = document.getElementById("airfoilDetailInfo");
+    let adf = document.getElementById("airfoilDetailInfo");
     if (data.warnMessage = undefined) {
         adf.innerHTML = warnMessage;
     }
     document.getElementById("name").setAttribute("value", data.name);
     document.getElementById("ShortName").setAttribute("value", data.shortName);
     document.getElementById("description").setAttribute("value", data.description);
-    var img_detail = document.getElementById("img_detail");
-    var img = document.createElement('img');
+    let img_detail = document.getElementById("img_detail");
+    let img = document.createElement('img');
     img.setAttribute("src", data.image);
     img_detail.appendChild(img);
 
 
-    var fileCsvName = data.fileCsvName;
-    var links = document.getElementById('graf');
-    for (var i = 0; i < fileCsvName.length; i++) {
-        var linkItem = document.createElement('a');
+    let fileCsvName = data.fileCsvName;
+    let links = document.getElementById('graf');
+    for (let i = 0; i < fileCsvName.length; i++) {
+        let linkItem = document.createElement('a');
         linkItem.setAttribute("href", "/resources/tmpCsv/" + fileCsvName[i].fileName);
         linkItem.innerHTML = fileCsvName[i].fileName;
         links.appendChild(linkItem);
@@ -170,12 +105,11 @@ function fillEditableContentDetailInfo(data) {
 
 function refreshiframe() {
     console.log("refreshiframe");
-    // var actions = document.querySelector('.csvItemChBox:checked');
-    var actions = document.getElementsByName("activ");
-    var checkeds = new Array(10);
-    for (var i = 0; i < actions.length; i++) {
+    let actions = document.getElementsByName("activ");
+    let checkeds = new Array(10);
+    for (let i = 0; i < actions.length; i++) {
         if (actions[i].checked) {
-            checkeds[i] = actions[i].id;
+            checkeds[i] = actions[i].getAttribute('filename');
         }
     }
     console.log(checkeds);
@@ -194,11 +128,11 @@ function refreshiframe() {
             success: function (data) {
                 console.log("SUCCESS: ", data);
                 document.getElementById("airfoilDetailInfo").removeChild(document.getElementById("imgCsvBox"));
-                var imgCsvBox = document.createElement('div');
+                let imgCsvBox = document.createElement('div');
                 imgCsvBox.id = "imgCsvBox";
-                for (var i = 0; i < data.length; i++) {
-                    var imgCsv = document.createElement('div');
-                    var img = document.createElement('img');
+                for (let i = 0; i < data.length; i++) {
+                    let imgCsv = document.createElement('div');
+                    let img = document.createElement('img');
                     img.setAttribute('src', data[i] + "?" + Math.random());
                     imgCsv.appendChild(img);
                     imgCsvBox.appendChild(imgCsv);
