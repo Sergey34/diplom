2f229e199afd1fad6f46d686ecd2d8e4b0bd3b34
sergey <sergey3440@gmail.com>
2017-01-07 15:36:23 +0400

    add CrossOrigin

--- src/main/java/net/sergey/diplom/rest/FullRestController.java
+++ src/main/java/net/sergey/diplom/rest/FullRestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin
 @RequestMapping(value = "/rest")
 @RestController
 public class FullRestController {
--- src/main/java/net/sergey/diplom/rest/RealTime.java
+++ src/main/java/net/sergey/diplom/rest/RealTime.java
@@ -2,12 +2,14 @@ package net.sergey.diplom.rest;
 
 import net.sergey.diplom.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 public class RealTime {
 
