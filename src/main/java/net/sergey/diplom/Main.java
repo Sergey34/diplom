89f6c35b3942ac8f122b35962a96e5b3991e7b47
seko0716 <sergey3440@gmail.com>
2017-02-02 13:44:01 +0400

    init (is not worked version)

--- src/main/java/net/sergey/diplom/Main.java
+++ src/main/java/net/sergey/diplom/Main.java
@@ -1,11 +1,50 @@
 package net.sergey.diplom;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Main {
+public class Main  extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+        @Autowired
+        DataSource dataSource;
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/getMenu").hasRole("ADMIN")
+                    .antMatchers("/getAirfoils").hasRole("USER")
+                    .antMatchers("/css/**").permitAll().anyRequest()
+                    .fullyAuthenticated().and().formLogin().loginPage("/login")
+                    .failureUrl("/login?error").permitAll().and().logout().permitAll();
+        }
+
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//            auth.jdbcAuthentication().dataSource(this.dataSource);
+             auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+        }
+
     }
 }
