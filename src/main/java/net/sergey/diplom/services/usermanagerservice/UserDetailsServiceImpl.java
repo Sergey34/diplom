5922354ab3c6bf2529d55b7986153a686c334c8c
sergey <sergey3440@gmail.com>
2017-03-07 19:47:03 +0400

    login

--- src/main/java/net/sergey/diplom/services/usermanagerservice/UserDetailsServiceImpl.java
+++ src/main/java/net/sergey/diplom/services/usermanagerservice/UserDetailsServiceImpl.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private final DaoUser daoUser;
+
     @Autowired
-    private DaoUser daoUser;
+    public UserDetailsServiceImpl(DaoUser daoUser) {
+        this.daoUser = daoUser;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
