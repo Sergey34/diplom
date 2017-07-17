8677df3b6c4027d71c1a282ad859602971b658db
seko0716 <sergey3440@gmail.com>
2017-07-17 09:33:51 +0400

    refactoring

--- src/main/java/net/sergey/diplom/domain/airfoil/Characteristics.java
+++ src/main/java/net/sergey/diplom/domain/airfoil/Characteristics.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @lombok.Data
@@ -14,9 +16,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @Document(collection = "characteristics")
 public class Characteristics {
-    private String coordinatesStl;
     @Id
+    private ObjectId id;
+    @Indexed(unique = true)
     private String fileName;
+    private String coordinatesStl;
     private double renolgs;
     private double nCrit;
     private double maxClCd;
@@ -24,9 +28,15 @@ public class Characteristics {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
-        if (!super.equals(o)) { return false; }
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
         Characteristics that = (Characteristics) o;
         return fileName != null ? fileName.equals(that.fileName) : that.fileName == null;
     }
